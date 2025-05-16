9c42cb7e5976f6307c1fb3a31b5e71582865394d
nataly-cunha-inteli <158575982+nataly-cunha-inteli@users.noreply.github.com>
2025-05-16 13:14:43 -0300

    feat: adiciona comentários no código

--- src/app/components/botao.tsx
+++ src/app/components/botao.tsx
@@ -1,11 +1,14 @@
-import { ReactElement } from "react";
-import { ArrowDownNarrowWide, PenLine } from "lucide-react";
+import { ReactElement } from "react"; // Import ReactElement do react, para utilização dos ícones
+import { ArrowDownNarrowWide, PenLine } from "lucide-react"; // Importa os ícones do lucide-react, que são os ícones de salvar e editar
 
+// Define a interface BotaoProps, que define as propriedades do componente Botao
 interface BotaoProps {
   text: 'Salvar' | 'Editar';
 }
 
+// Define o componente Botao, que recebe as propriedades BotaoProps
 export default function Botao({ text }: BotaoProps) {
+  // Condicional para definir qual ícone será exibido com base no texto passado como propriedade
   const icon: ReactElement =
     text === "Salvar" ? (
       <ArrowDownNarrowWide className="w-4 h-4" />
@@ -14,6 +17,7 @@ export default function Botao({ text }: BotaoProps) {
     );
 
   return (
+    // Retorna o botão com as classes CSS definidas, o ícone e o texto
     <button
       className="flex min-w-[101px] max-w-[180px] w-full px-4 py-3 justify-center items-center gap-2 self-stretch 
                  rounded border border-[#CE3813] bg-[#CE6A51] text-white hover:bg-[#CE3813] transition-colors"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+// Importa o componente Botao
 import Botao from "./components/botao";
 
+// Exporta o componente padrão da Home
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen p-24">
-      <Botao text="Salvar" />
+    <div className="flex flex-col items-center justify-center h-screen w-screen p-24"> {/* CSS interno da div onde estarão os botões */}
+      
+      <Botao text="Salvar" /> {/* Assim como a condicional definida dentro de components/botao, se o texto é "Salvar", aparece o botão de salvar. Mesmo caso para "Editar" */}
       <Botao text="Editar" />
     </div>
   );
